8c68fcd56acc98ed7c6ef045f81ef92e0b572a24
webonly-designs <webonly.designs@gmail.com>
2023-07-18 17:28:37 -0500

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+   <div>
+      <header>
+        Ts Amazona
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        All rights reserved
+      </footer>
+   </div>
+  )
+}
+
+export default App
